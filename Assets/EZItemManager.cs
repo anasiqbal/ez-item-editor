d23a87beca83d0799498d5397f7d044109ac1f38
Celeste Burke <celeste@stayathomedevs.com>
2014-04-01 17:22:44 -0400

    Added the item manager and some test items to it. Item manager window now loops through the items and draws each one.

---
+++ Assets/EZItemManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EZItemManager
+{
+    public static Dictionary<string, object> AllItems { private set; get; }
+}
--- Assets/Editor/EZItemManagerWindow.cs
+++ Assets/Editor/EZItemManagerWindow.cs
@@ -1,20 +1,68 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EZItemManagerWindow : EditorWindow
 {
     private const string menuItemLocation = "Assets/EZ Item Manager";
+    private static Dictionary<string, object> itemDictionary;
 
     [MenuItem(menuItemLocation)]
     private static void showEditor()
     {
         EditorWindow.GetWindow<EZItemManagerWindow>(false, "EZ Item Manager");
+
+        itemDictionary = EZItemManager.AllItems;
     }
 
     [MenuItem(menuItemLocation, true)]
     private static bool showEditorValidator()
     {
         return true;
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.LabelField("All Items");
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.EndHorizontal();
+
+        TestMe();
+
+        foreach (object item in itemDictionary.Values)
+        {
+            DrawItem(item);
+        }
+    }
+
+    private static void TestMe()
+    {
+        if (itemDictionary == null)
+        {
+            itemDictionary = new Dictionary<string, object>();
+            itemDictionary.Add("1", "Item details go here....");
+            itemDictionary.Add("2", "Item details go here....");
+            itemDictionary.Add("3", "Item details go here....");
+        }
+    }
+
+    private static void DrawItem(object data)
+    {
+        EditorGUILayout.BeginVertical();
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("An Item");
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.LabelField(data as string);
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
     }
 }
