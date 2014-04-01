6a3599e260f12833f7d86e3425ecfc9678d44087
Celeste Burke <celeste@stayathomedevs.com>
2014-04-01 16:17:48 -0400

    Added an empty editor window. Added .gitignore with the defaults for Unity/Mono generated files

---
+++ Assets/Editor/EZItemManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class EZItemManager : EditorWindow
+{
+    private const string menuItemLocation = "Assets/EZ Item Manager";
+
+    [MenuItem(menuItemLocation)]
+    private static void showEditor()
+    {
+        EditorWindow.GetWindow<EZItemManager>(false, "EZ Item Manager");
+    }
+
+    [MenuItem(menuItemLocation, true)]
+    private static bool showEditorValidator()
+    {
+        return true;
+    }
+}
