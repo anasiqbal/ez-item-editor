aa964c649e682fef63ed54067b1d5d6e970573d9
Celeste Burke <celeste@stayathomedevs.com>
2014-06-11 16:04:25 -0400

    Added initial playmaker actions for initializing the gde manager and getting a string from an item.

---
+++ Assets/GameDataEditorPlaymaker/GDEGetString.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using GameDataEditor;
+using GameDataEditor.GDEExtensionMethods;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("Game Data Editor")]
+    [Tooltip("Gets a String from a GDE Item")]
+    public class GDEGetString : FsmStateAction
+    {
+        [RequiredField]
+        [UIHint(UIHint.FsmString)]
+        [Tooltip("Item Name")]
+        public FsmString itemName;
+
+        [RequiredField]
+        [UIHint(UIHint.FsmString)]
+        [Tooltip("Field Name")]
+        public FsmString fieldName;
+
+        [UIHint(UIHint.FsmString)]
+        public FsmString storeResult;
+
+        public override void Reset()
+        {
+            itemName = null;
+            fieldName = null;
+            storeResult = null;
+        }
+        
+        public override void OnEnter()
+        {
+            try
+            {
+                Dictionary<string, object> data;
+                if (GameDataEditor.GDEDataManager.Instance.Get(itemName.Value, out data))
+                {
+                    string val;
+                    data.TryGetString(fieldName.Value, out val);
+                    storeResult.Value = val;
+                }
+            }
+            catch(UnityException ex)
+            {
+                Debug.LogException(ex);
+            }
+            finally
+            {
+                Finish();       
+            }
+        }
+    }
+}
---
+++ Assets/GameDataEditorPlaymaker/GDEManagerInit.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using GameDataEditor;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("Game Data Editor")]
+    [Tooltip("Initializes the Game Data Manager")]
+    public class GDEManagerInit : FsmStateAction
+    {
+        [RequiredField]
+        [UIHint(UIHint.FsmString)]
+        [Tooltip("GDE Data File Name")]
+        public FsmString gdeDataFileName;
+
+        public override void Reset()
+        {
+            gdeDataFileName = null;
+        }
+        
+        public override void OnEnter()
+        {
+            try
+            {
+                GDEDataManager.Instance.Init(gdeDataFileName.Value);
+            }
+            catch(UnityException ex)
+            {
+                Debug.LogException(ex);
+            }
+            finally
+            {
+                Finish();       
+            }
+        }
+    }
+}
